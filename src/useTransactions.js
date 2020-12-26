6921550f22f864ff2ba9af49cb507f26cea4776d
Akash52 <ac8572611@gmail.com>
2020-12-26 10:50:19 +0530

    JSON UPDATED

---
+++ src/useTransactions.js
@@ -0,0 +1,14 @@
+import { useContext } from 'react'
+import { ExpenseTrackerContext } from './context/context'
+
+import {
+  incomeCategories,
+  expenseCategories,
+  resetCategories,
+} from './constants/categories'
+
+const useTransactions = (title) => {
+  resetCategories()
+
+  const { transactions } = useContext(ExpenseTrackerContext)
+}
